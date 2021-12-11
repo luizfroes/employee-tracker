b6b099b3cd515878d51766ad3df2c576d8f4a203
Luiz Carlos Froes <luizfroes@gmail.com>
2021-12-11 03:04:25 +0000

    Added all if statements on index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const inquirer = require("inquirer");
+const { openingQuestion } = require("./questions");
+
 const displayDepartments = () => {
   // execute mysql query
   // log/table departments
@@ -52,26 +55,106 @@ const constructEmployeeChoices = (roles) => {
 //   console.log(`Connected to the classlist_db database.`)
 // );
 
-const start = () => {
-  // declare one question with list of actions
+const start = async () => {
   // prompt question and get answer (action)
+  const chosenAction = await inquirer.prompt(openingQuestion);
+
   // insert if blocks for all actions
-  // if displayDepartments()
-  // if displayRoles()
-  // if displayEmployees()
-  if ('addDepartment') {
+  if ("addDepartment") {
     // prompt department questions (name) and get answers
     // construct mysql insert query
     // execute mysql query
   }
-  if ('addRole') {
+
+  if ("addRole") {
     // get departments from DB
     // pass the departments to a choice constructor function
     // prompt question to select department, title, salary and get answers
     // construct mysql insert query for role
     // execute mysql query
   }
-  if ('addEmployee') {
+
+  if ("addAEmployee") {
+    // get roles from DB
+    // get employees from DB
+    // pass the roles to a choice constructor function
+    // pass the employees to a choice constructor function
+    // prompt question to select role, select manager, first name, last name and get answers
+    // construct mysql insert query for employee
+    // execute mysql query
+  }
+
+  if ("deleteARole") {
+    // prompt department questions (name) and get answers
+    // construct mysql insert query
+    // execute mysql query
+  }
+
+  if ("deleteADepartment") {
+    // prompt department questions (name) and get answers
+    // construct mysql insert query
+    // execute mysql query
+  }
+
+  if ("deleteAEmployee") {
+    // prompt department questions (name) and get answers
+    // construct mysql insert query
+    // execute mysql query
+  }
+
+  if ("updateEmployeeRole") {
+    // prompt department questions (name) and get answers
+    // construct mysql insert query
+    // execute mysql query
+  }
+
+  if ("updateEmployeeManagers") {
+    // prompt department questions (name) and get answers
+    // construct mysql insert query
+    // execute mysql query
+  }
+
+  if ("viewAllDepartments") {
+    // get departments from DB
+    // pass the departments to a choice constructor function
+    // prompt question to select department, title, salary and get answers
+    // construct mysql insert query for role
+    // execute mysql query
+  }
+
+  if ("viewAllRoles") {
+    // prompt department questions (name) and get answers
+    // construct mysql insert query
+    // execute mysql query
+  }
+
+  if ("viewAllEmployees") {
+    // prompt department questions (name) and get answers
+    // construct mysql insert query
+    // execute mysql query
+  }
+
+  if ("viewEmployeesByManager") {
+    // prompt department questions (name) and get answers
+    // construct mysql insert query
+    // execute mysql query
+  }
+
+  if ("viewEmployeesByDepartment") {
+    // prompt department questions (name) and get answers
+    // construct mysql insert query
+    // execute mysql query
+  }
+
+  if ("viewDepartmentBudget") {
+    // get departments from DB
+    // pass the departments to a choice constructor function
+    // prompt question to select department, title, salary and get answers
+    // construct mysql insert query for role
+    // execute mysql query
+  }
+
+  if ("addEmployee") {
     // get roles from DB
     // get employees from DB
     // pass the roles to a choice constructor function
