ad06fa51da41adf9799d46ff9fd9a38254f8873e
Luiz Carlos Froes <luizfroes@gmail.com>
2021-12-12 03:17:26 +0000

    Refactored some utils functions

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,38 +1,60 @@
-const displayDepartments = () => {
+const displayDepartments = async (db) => {
   // execute mysql query
-  const departments = await db.query("SELECT * FROM departments");
+  const departments = await db.query("SELECT * FROM department");
 
   // log/table departments
   return console.table(departments);
 };
 
-const displayRoles = () => {
+const displayRoles = async (db) => {
   // execute mysql query
-  const roles = await db.query("SELECT * FROM roles");
+  const roles = await db.query(
+    "SELECT role.id, role.title, role.salary, department.name FROM role JOIN department ON role.departmentId = department.id ORDER BY department.name;"
+  );
 
   // log/table roles
   return console.table(roles);
 };
 
-const displayEmployees = () => {
+const displayEmployees = async (db) => {
   // execute mysql query
-  const employees = await db.query("SELECT * FROM employees");
+  const employees = await db.query(
+    "SELECT employee_role.firstName, employee_role.lastName, title, salary, name FROM employee employee_role LEFT JOIN role ON employee_role.roleId=role.id LEFT JOIN department ON role.departmentId=department.id"
+  );
 
   // log/table employees
   return console.table(employees);
 };
 
-const getDepartments = () => {
+const displayEmployeesByManager = async (db) => {
+  // execute mysql query
+  const employees = await db.query("SELECT * FROM employee ORDER BY ");
+
+  // log/table employees
+  return console.table(employees);
+};
+
+const displayEmployeesByDepartment = async (db) => {
+  // execute mysql query
+  const employees = await db.query(
+    "SELECT employee_role.firstName, employee_role.lastName, title, salary, name FROM employee employee_role LEFT JOIN role ON employee_role.roleId=role.id LEFT JOIN department ON role.departmentId=department.id ORDER BY department.name;"
+  );
+
+  // log/table employees
+  return console.table(employees);
+};
+
+const getDepartments = (db) => {
   // execute mysql query
   // return departments
 };
 
-const getRoles = () => {
+const getRoles = (db) => {
   // execute mysql query
   // return roles
 };
 
-const getEmployees = () => {
+const getEmployees = (db) => {
   // execute mysql query
   // return employees
 };
